41797636fe6748afbea066483078fbb3e9b13736
João Lenon <jlenonweb@gmail.com>
2013-01-14 21:48:12 -0200

    Added PHP_EOL to break lines

--- src/ProjectBuilder/Output.php
+++ src/ProjectBuilder/Output.php
@@ -6,12 +6,12 @@ class Output
 {
 	public static function outputString($string)
 	{
-		echo $string ."\n";
+		echo $string . PHP_EOL;
 	}
 
 	public static function outputError($num, $message, $file = 'project-builder', $line = 1)
 	{
-		echo '----------------------------- Error -----------------------------';
-		echo "ERR$num: $message on $file, line $line";
+		echo '----------------------------- Error -----------------------------' . PHP_EOL;
+		echo "ERR$num: $message on $file, line $line" . PHP_EOL;
 	}
 }
